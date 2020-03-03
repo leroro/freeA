ecedd2be559ec98424c84d6453e250f13500b5dd
임미선 <leroro@inseq.co.kr>
2020-03-03 14:21:55 +0900

    홈화면용 아이콘 추가

--- assets/js/ui-layout-setting.js
+++ assets/js/ui-layout-setting.js
@@ -1,9 +1,3 @@
-/* 
-	본 JS는 퍼블리싱 편의를 위해 
-	중복되는 공통 레이아웃 영역을 로드할 목적으로 작성된 문서입니다.
-	서버 언어로 레이아웃을 구성하게되면 오류를 유발하는 코드이오니
-	Back-End 개발시 이 파일을 반드시 삭제해 주세요 :)
-*/
 $(function () {
 	$('#titlebar').load('inc-titlebar.html', function () {});
 	// $('#gnb').load('_inc-gnb.html', function () {});
