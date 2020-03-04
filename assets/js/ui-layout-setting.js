48fdee82c79feea4b2078dab6fcdb75626c635bf
임미선 <leroro@inseq.co.kr>
2020-03-04 11:49:28 +0900

    통계 데이터 추가

--- assets/js/ui-layout-setting.js
+++ assets/js/ui-layout-setting.js
@@ -1,18 +1,15 @@
 $(function () {
 	$('#titlebar').load('inc-titlebar.html', function () {});
-	// $('#gnb').load('_inc-gnb.html', function () {});
-	// 	if ($('#header').hasClass('extend-open')) {
-	// 		$('.extend-container').show();
-	// 	}
-
-	// $('#allmenu').load('_inc-allmenu.html', function () {
-	// 	// if ($.isFunction(window.snbSetting)) snbSetting();
-	// });
 	$('#tabbar').load('inc-tabbar.html', function () {
 		var thisfilefullname = document.URL.substring(document.URL.lastIndexOf("/") + 1, document.URL.length);
+		console.log(thisfilefullname);
+
 		$('#tabbar a').each(function () {
 			if ($(this).attr('href') == thisfilefullname) {
 				$(this).parent('li').addClass('on');
+			} else if (thisfilefullname == '') {
+				// 인덱스
+				$('#tabbar li:first-child').addClass('on');
 			}
 		});
 	});
